ef9a8a42a0fd9b5c0366165710f1349b8549f876
Bibiboo80 <Bibiboo80@users.noreply.github.com>
2023-03-28 18:24:39 +0200

    der will mir nichts sagen :-(

--- static/js/speech.js
+++ static/js/speech.js
@@ -1,26 +1,35 @@
 /**
  * speak
- * 
+ *
  * Diese Funktion übernimmt einen Text und verwendet die Web Speech API,
  * um den Text laut vorzulesen.
- * 
- * @param {*} text 
+ *
+ * @param {*} text
  */
+
 async function speak(text) {
-  // TODO
+    if ('speechSynthesis' in window) {
+        const utterance = new SpeechSynthesisUtterance(text);
+        utterance.lang = 'de-DE';
+        utterance.rate = 1.5;
+        speechSynthesis.speak(utterance);
+    } else {
+        console.error('Web Speech API wird nicht unterstützt');
+    }
 }
+
 
 /**
  * Dieser Code muss nicht verändert werden. Er sorgt dafür, dass das
  * Test-Formular im Browser funktioniert und die Funktion speak
  * ausführt.
  */
-document.addEventListener('DOMContentLoaded', function() {
-  const speechForm = document.querySelector('#speechForm');
-  const speedInput = document.querySelector('#speechInput');
+document.addEventListener('DOMContentLoaded', function () {
+    const speechForm = document.querySelector('#speechForm');
+    const speedInput = document.querySelector('#speechInput');
 
-  speechForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    await speak(speedInput.value);
-  });
+    speechForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        await speak(speedInput.value);
+    });
 });
